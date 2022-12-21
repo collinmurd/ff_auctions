b3b287e3e770cee64d75f743c660689860849bdb
Collin Murdock <murdoccr@miamioh.edu>
2022-12-20 23:31:01 -0500

    basic header

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,10 +1,16 @@
 import Header from './Header';
+import './App.css'
+
+const user = {
+  name: 'Collin',
+  balance: 420
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header />
+        <Header user={user} />
       </header>
     </div>
   );
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
+import './Header.css';
 
+// props: {
+//     user: {
+//         name: 'user name'
+//         balance: balance remaining
+//     }
+// }
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
-            <div>
-                <h1>Auction Portal</h1>
+            <div class="header">
+                <h1 class="title">Auction Portal</h1>
+                <div class="userInfo">
+                    <h3>{this.props.user.name}</h3>
+                    <h5>Cash Remaining: ${this.props.user.balance}</h5>
+                </div>
             </div>
         );
     }
