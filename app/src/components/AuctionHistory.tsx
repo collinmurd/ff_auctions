6ad73210581b1606f159dce4b472d51c02d1525c
Collin Murdock <murdoccr@miamioh.edu>
2022-12-21 11:32:35 -0500

    add sample data

--- app/src/components/AuctionHistory.tsx
+++ app/src/components/AuctionHistory.tsx
@@ -2,6 +2,30 @@
 
 import React from "react";
 
+const managersData = [
+    {manager: 'Collin', cashRemaining: 420, weekEliminated: -1},
+    {manager: 'Spencer', cashRemaining: 250, weekEliminated: -1},
+    {manager: 'Nick', cashRemaining: 100, weekEliminated: 1}
+]
+
+const auctionsData = [
+    {
+        week: 1,
+        wins: [
+            {manager: 'Collin', player: 'Cooper Kupp', amount: 400},
+            {manager: 'Nick', player: 'Justin Jefferson', amount: 900},
+        ]
+    },
+    {
+        week: 2,
+        wins: [
+            {manager: 'Collin', player: 'Nick Chubb', amount: 180},
+            {manager: 'Spencer', player: 'Justin Jefferson', amount: 500},
+            {manager: 'Spencer', player: 'Tyreek Hill', amount: 250}
+        ]
+    },
+]
+
 type AuctionHistoryTypes = {
 
 };
