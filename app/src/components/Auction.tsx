87b8f9ab8e6e3b9a0b05f02b5ee804116ecce826
Collin Murdock <collinmurdock22@gmail.com>
2022-12-24 16:58:13 -0500

    styled auction cards

--- app/src/components/Auction.tsx
+++ app/src/components/Auction.tsx
@@ -4,6 +4,29 @@ import './Auction.css'
 
 import React from "react";
 
+const data = [
+    {
+        "playerName": "Kyler Murray",
+        "position": "QB"
+    },
+    {
+        "playerName": "Nick Chubb",
+        "position": "RB"
+    },
+    {
+        "playerName": "Derrick Henry",
+        "position": "RB"
+    },
+    {
+        "playerName": "Justin Jefferson",
+        "position": "WR"
+    },
+    {
+        "playerName": "Cleveland Defense",
+        "position": "DEF"
+    }
+];
+
 type AuctionProps = {};
 export class Auction extends React.Component<AuctionProps> {
     constructor(props: AuctionProps) {
@@ -15,34 +38,49 @@ export class Auction extends React.Component<AuctionProps> {
             <div className="auction">
                 <h2>Submit Your Bids</h2>
                 <div className="auctionSectionContainer">
-                    <AuctionPositionSection position="QB" />
-                    <AuctionPositionSection position="WR" />
-                    <AuctionPositionSection position="RB" />
-                    <AuctionPositionSection position="TE" />
-                    <AuctionPositionSection position="K" />
-                    <AuctionPositionSection position="DEF" />
+                    <AuctionPositionSection position="QB" players={this.getPlayersByPosition("QB")} />
+                    <AuctionPositionSection position="WR" players={this.getPlayersByPosition("WR")} />
+                    <AuctionPositionSection position="RB" players={this.getPlayersByPosition("RB")} />
+                    <AuctionPositionSection position="TE" players={this.getPlayersByPosition("TE")} />
+                    <AuctionPositionSection position="K" players={this.getPlayersByPosition("K")} />
+                    <AuctionPositionSection position="DEF" players={this.getPlayersByPosition("DEF")} />
                 </div>
             </div>
         );
+    }
+
+    getPlayersByPosition(position: string) {
+        return data.filter((player) => {
+            return player.position === position;
+        }).map((player) => {
+            return player.playerName;
+        });
     }
 }
 
 // ----------------------------------------------
 
 type AuctionPositionSectionProps = {
-    position: string
+    position: string,
+    players: string[]
 };
 class AuctionPositionSection extends React.Component<AuctionPositionSectionProps> {
+    playerCardElements: JSX.Element[];
     constructor(props: AuctionPositionSectionProps) {
         super(props);
+
+        this.playerCardElements = this.props.players.map((player) => {
+            return <AuctionPlayerCard playerName={player} key={player} />
+        });
     }
 
     render() {
         return (
             <div className="auctionPositionSection">
-                <h3>{this.props.position}</h3>
-                <AuctionPlayerCard playerName="Nick Chubb" />
-                <AuctionPlayerCard playerName="Justin Jefferson" />
+                <h3 className="positionTitle">{this.props.position}</h3>
+                <div className="auctionPlayerCardContainer">
+                    {this.playerCardElements.length > 0 ? this.playerCardElements : <p className="noPlayers">No players.</p>}
+                </div>
             </div>
         );
     }
