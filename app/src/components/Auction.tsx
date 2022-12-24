a7e56b5c7a60f43198f3e1e9ff56dcb49ada660f
Collin Murdock <collinmurdock22@gmail.com>
2022-12-24 14:26:24 -0500

    auction sections by position

--- app/src/components/Auction.tsx
+++ app/src/components/Auction.tsx
@@ -1,19 +1,48 @@
 // Component that contains the auction form
 
+import './Auction.css'
+
 import React from "react";
 
-type AuctionTypes = {
-
-};
-
-export class Auction extends React.Component<AuctionTypes> {
-    constructor(props: AuctionTypes) {
+type AuctionProps = {};
+export class Auction extends React.Component<AuctionProps> {
+    constructor(props: AuctionProps) {
         super(props);
     }
 
     render() {
         return (
-            <h2>This is where the Auction form will go</h2>
-        )
+            <div className="auction">
+                <h2>Submit Your Bids</h2>
+                <div className="auctionSectionContainer">
+                    <AuctionPositionSection position="QB" />
+                    <AuctionPositionSection position="WR" />
+                    <AuctionPositionSection position="RB" />
+                    <AuctionPositionSection position="TE" />
+                    <AuctionPositionSection position="K" />
+                    <AuctionPositionSection position="DEF" />
+                </div>
+            </div>
+        );
+    }
+}
+
+// ----------------------------------------------
+
+type AuctionPositionSectionProps = {
+    position: string
+};
+export class AuctionPositionSection extends React.Component<AuctionPositionSectionProps> {
+    constructor(props: AuctionPositionSectionProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="auctionPositionSection">
+                <h3>{this.props.position}</h3>
+                <p>No players</p>
+            </div>
+        );
     }
 }
