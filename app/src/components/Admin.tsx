6d6866f53ee65bca87ae01b7589749da4081c161
Collin Murdock <collinmurdock22@gmail.com>
2022-12-26 18:59:09 -0500

    just two buttons

--- app/src/components/Admin.tsx
+++ app/src/components/Admin.tsx
@@ -4,8 +4,6 @@ import './Admin.css'
 
 import React from "react";
 
-const currentSeason = "2022-2023";
-
 const managers: {[season: string]: string[]} = {
     "2022-2023": [
         "Collin",
@@ -20,7 +18,8 @@ const managers: {[season: string]: string[]} = {
     ]
 }
 
-const currentWeek = 4;
+const currentSeason = "2022-2023"; // can be null. that would indicate there is no current season
+const currentWeek = 4; // can be null. indicates same as above
 const existingAuction = false;
 
 type AdminProps = {};
@@ -36,8 +35,29 @@ export class Admin extends React.Component<AdminProps, AdminState> {
         return (
             <div id="admin">
                 <h2>Administration</h2>
-                <h4>Current Season: {currentSeason}</h4>
+                <h4>
+                    {currentSeason ? `Current Season: ${currentSeason}` : 'No Current Season'}
+                </h4>
+                <h4>
+                    {currentWeek ? `Current Week: ${currentWeek}` : 'No Current Season'}
+                </h4>
+                <div className="buttonContainer">{this.seasonButton()}</div>
+                <div className="buttonContainer">{this.auctionButton()}</div>
             </div>
         );
+    }
+
+    seasonButton() {
+        if (currentSeason)
+            return <button id="createSeasonButton" className="button">Create New Season</button>
+        else
+            return <button id="endSeasonButton" className="button">End Current Season</button>
+    }
+
+    auctionButton() {
+        if (currentSeason)
+            return <button id="createAuctionButton" className="button">Create New Auction</button>
+        else
+            return <button id="endAuctionButton" className="button">End Current Auction</button>
     }
 }
